9027ec88849276ea38974172cff85fef9f678de0
ODale <odalecotterell@hotmail.com>
2017-09-19 22:12:25 -0400

    added text field to main view and viewcontroller

---
+++ testAppIOS/MyViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CoreGraphics;
+
+using UIKit;
+
+namespace testAppIOS
+{
+    public partial class MyViewController : UIViewController
+    {
+        public MyViewController() : base("MyViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            this.View.BackgroundColor = UIColor.Yellow;
+
+            var nameFieldTxt = new UITextField()
+            {
+                Frame = new CGRect(20, 28, View.Bounds.Width - 40, 35),
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Enter Your Name Here",
+            };
+
+            View.Add(nameFieldTxt);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
